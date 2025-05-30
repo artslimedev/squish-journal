7d5cd20cd83a9395ef381f54f3063e92b462d992
artslimedev <102567983+artslimedev@users.noreply.github.com>
2025-05-30 16:14:45 -0400

    Updating code and dependencies

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
-const PORT = 6969;
+const PORT = process.env.PORT || 6969;
 const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
+const MongoStore = require('connect-mongo')
 const flash = require('express-flash')
 const logger = require('morgan')
 const connectDB = require('./config/database')
@@ -30,7 +30,7 @@ app.use(
       secret: 'keyboard cat',
       resave: false,
       saveUninitialized: false,
-      store: new MongoStore({ mongooseConnection: mongoose.connection }),
+      store: MongoStore.create({ mongoUrl: process.env.DB_STRING }),
     })
   )
   
@@ -50,4 +50,4 @@ app.use('/dashboard', dashboardRoutes)
  
 app.listen(process.env.PORT || PORT, () => {
   console.log(`Server is running on port ${PORT}`)
-})   
+})
