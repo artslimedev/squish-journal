e286cac35ac49513e32d145fe5941201a83b9bc4
artslimedev <102567983+artslimedev@users.noreply.github.com>
2025-05-30 16:31:52 -0400

    Updating code and dependencies

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,39 +11,44 @@ const User = require('../models/User')
     })
   }
   
-  exports.postLogin = (req, res, next) => {
-    const validationErrors = []
-    if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' })
-    if (validator.isEmpty(req.body.password)) validationErrors.push({ msg: 'Password cannot be blank.' })
+  exports.postLogin = async (req, res, next) => {
+    try {
+      const validationErrors = []
+      if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' })
+      if (validator.isEmpty(req.body.password)) validationErrors.push({ msg: 'Password cannot be blank.' })
   
-    if (validationErrors.length) {
-      req.flash('errors', validationErrors)
-      return res.redirect('/login')
-    }
-    req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false })
-  
-    passport.authenticate('local', (err, user, info) => {
-      if (err) { return next(err) }
-      if (!user) {
-        req.flash('errors', info)
+      if (validationErrors.length) {
+        req.flash('errors', validationErrors)
         return res.redirect('/login')
       }
-      req.logIn(user, (err) => {
+      req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false })
+  
+      passport.authenticate('local', async (err, user, info) => {
         if (err) { return next(err) }
-        req.flash('success', { msg: 'Success! You are logged in.' })
-        res.redirect(req.session.returnTo || '/dashboard')
-      })
-    })(req, res, next)
+        if (!user) {
+          req.flash('errors', info)
+          return res.redirect('/login')
+        }
+        
+        req.logIn(user, (err) => {
+          if (err) { return next(err) }
+          req.flash('success', { msg: 'Success! You are logged in.' })
+          res.redirect(req.session.returnTo || '/dashboard')
+        })
+      })(req, res, next)
+    } catch (err) {
+      return next(err)
+    }
   }
   
-  exports.logout = (req, res) => {
-    req.logout(() => {
-      console.log('User has logged out.')
-    })
-    req.session.destroy((err) => {
-      if (err) console.log('Error : Failed to destroy the session during logout.', err)
-      req.user = null
-      res.redirect('/')
+  exports.logout = (req, res, next) => {
+    req.logout(function(err) {
+      if (err) { return next(err) }
+      req.session.destroy((err) => {
+        if (err) console.log('Error : Failed to destroy the session during logout.', err)
+        req.user = null
+        res.redirect('/')
+      })
     })
   }
   
--- models/User.js
+++ models/User.js
@@ -2,44 +2,30 @@ const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
-
-  userName: {    
-    type: String,
-    unique: true, 
-    required: true
-  },    
-  email: { 
-    type: String, 
-    unique: true,
-    required: true
-   },
+  userName: { type: String, unique: true },
+  email: { type: String, unique: true },
   password: String
 })
 
-
-// Password hash middleware.
- 
- UserSchema.pre('save', function save(next) {
-  const user = this
-  if (!user.isModified('password')) { return next() }
-  bcrypt.genSalt(10, (err, salt) => {
-    if (err) { return next(err) }
-    bcrypt.hash(user.password, salt, (err, hash) => {
-      if (err) { return next(err) }
-      user.password = hash
-      next()
-    })
-  })
+// Password hash middleware
+UserSchema.pre('save', async function(next) {
+  try {
+    if (!this.isModified('password')) return next()
+    const hash = await bcrypt.hash(this.password, 10)
+    this.password = hash
+    next()
+  } catch (err) {
+    next(err)
+  }
 })
 
-
-// Helper method for validating user's password.
-
-UserSchema.methods.comparePassword = function comparePassword(candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
-    cb(err, isMatch)
-  })
+// Helper method for validating user's password
+UserSchema.methods.comparePassword = async function(candidatePassword) {
+  try {
+    return await bcrypt.compare(candidatePassword, this.password)
+  } catch (err) {
+    throw err
+  }
 }
-
 
 module.exports = mongoose.model('User', UserSchema)
