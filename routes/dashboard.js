944ab8e062dd139e9ec9ea36b8c8306ea5861a02
artslimedev <102567983+artslimedev@users.noreply.github.com>
2025-05-31 14:43:55 -0400

    cleaning up code and UI

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,15 +1,13 @@
-const express = require('express')
-const router = express.Router()
-const authController = require('../controllers/auth') 
-const dashboardController = require('../controllers/dashboard')
-const homeController = require('../controllers/home')
-const { ensureAuth, ensureGuest } = require('../middleware/auth')
+const express = require("express");
+const router = express.Router();
+const dashboardController = require("../controllers/dashboard");
+const homeController = require("../controllers/home");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
-
-router.get('/', ensureAuth, dashboardController.getEntries)
-router.get('/', ensureAuth, homeController.getJournal)
-router.post('/entry', dashboardController.createEntry)
+router.get("/", ensureAuth, dashboardController.getEntries);
+router.get("/", ensureAuth, homeController.getJournal);
+router.post("/entry", dashboardController.createEntry);
 
 // router.delete('/deleteTodo', entriesController.deleteEntry)
 
-module.exports = router
+module.exports = router;
